2423829bcaeb994f53be0de1214c57592d971f76
Sébastien Willmann <sebastien.willmann@gmail.com>
2015-10-18 14:47:09 +0200

    Added turn capability

--- src/main/java/willmann/mower/Mower.java
+++ src/main/java/willmann/mower/Mower.java
@@ -1,30 +1,51 @@
 package willmann.mower;
 
+/**
+ * This class represents a Mower, with its position and orientation.
+ *
+ * This class is immutable.
+ */
 public final class Mower {
     private final int x;
     private final int y;
     private final Orientation orientation;
 
+    /**
+     * Create a new Mower with the specified x, y and orientation.
+     */
     public Mower(final int x, final int y, final Orientation orientation) {
         this.x = x;
         this.y = y;
         this.orientation = orientation;
     }
 
+    /**
+     * @return the x position of this mower
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * @return the y position of this mower
+     */
     public int getY() {
         return this.y;
     }
 
+    /**
+     * @return the direction this mower faces.
+     */
     public Orientation getOrientation() {
         return this.orientation;
     }
 
+    /**
+     * Makes the mower move forward. A new mower is returned, this mower stay unchanged. The new mower will have the same orientation as the original.
+     * @return a new Mower moved one square from the original mower, in the direction corresponding to the orientation.
+     */
     public Mower moveForward() {
-        int newX = this.x, newY = this.y;
+        int newX = this.getX(), newY = this.getY();
         switch(this.orientation) {
             case NORTH:
                 newY++;
@@ -39,13 +60,72 @@ public final class Mower {
                 newX--;
                 break;
         }
-        return new Mower(newX, newY, this.orientation);
+        return new Mower(newX, newY, getOrientation());
+    }
+    
+    /**
+     * Makes the mower turn left.
+     * @return a new mower with the same position, turned left.
+     */
+    public Mower turnLeft() {
+        return new Mower(getX(), getY(), getOrientation().left);
     }
 
+    /**
+     * Makes the mower turn right.
+     * @return a new mower with the same position, turned right.
+     */
+    public Mower turnRight() {
+        return new Mower(getX(), getY(), getOrientation().right);
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if(!(object instanceof Mower)) {
+            return false;
+        }
+        final Mower other = (Mower) object;
+        return 
+            this.x == other.x &&
+            this.y == other.y &&
+            this.orientation == other.orientation;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = result * prime + this.x;
+        result = result * prime + this.y;
+        result = result * prime + this.orientation.hashCode();
+        return result;
+    }
+
+    /**
+     * Enumeration corresponding to the possible directions faced by a mower.
+     */
     public enum Orientation {
         NORTH,
         SOUTH,
         EAST,
         WEST;
+
+        static {
+            NORTH.left = WEST;
+            NORTH.right = EAST;
+
+            SOUTH.left = EAST;
+            SOUTH.right = WEST;
+
+            EAST.left = NORTH;
+            EAST.right = SOUTH;
+
+            WEST.left = SOUTH;
+            WEST.right = NORTH;
+        }
+
+        private Orientation left;
+        private Orientation right;
+
     }
 }
--- src/test/java/willmann/mower/MowerTest.java
+++ src/test/java/willmann/mower/MowerTest.java
@@ -6,16 +6,29 @@ import org.junit.Test;
 import willmann.mower.Mower;
 import willmann.mower.Mower.Orientation;
 
+/**
+ * Tests for the mower class
+ */
 public final class MowerTest {
 
     @Test
     public void moveForward() {
-        final Mower start = new Mower(0, 0, Orientation.NORTH);
-        final Mower end = start.moveForward();
+        final Mower mower = new Mower(0, 0, Orientation.NORTH).moveForward();
 
-        assertEquals(0, end.getX());
-        assertEquals(1, end.getY());
-        assertEquals(Orientation.NORTH, end.getOrientation());
+        assertEquals(new Mower(0, 1, Orientation.NORTH), mower);
     }
 
+    @Test
+    public void turnLeft() {
+        final Mower mower = new Mower(0, 0, Orientation.NORTH).turnLeft();
+
+        assertEquals(new Mower(0, 0, Orientation.WEST), mower);
+    } 
+
+    @Test
+    public void turnRight() {
+        final Mower mower = new Mower(0, 0, Orientation.NORTH).turnRight();
+
+        assertEquals(new Mower(0, 0, Orientation.EAST), mower);
+    } 
 }
